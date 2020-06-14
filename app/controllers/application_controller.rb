cfe2955b696a848303a1c91879f76d3abd07eb4a
Allan Farca <allan.farca@gmail.com>
2020-06-14 04:21:09 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,5 @@
 class ApplicationController < Sinatra::Base
 
-class Dog
-  attr_accessor :name, :breed, :age
-
-  @@all = []
-
-  def initialize (name, breed, age)
-    @name = name
-    @breed = breed
-    @age = age
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-
-end
-
   configure do
   	set :views, "app/views"
   	set :public_dir, "public"
